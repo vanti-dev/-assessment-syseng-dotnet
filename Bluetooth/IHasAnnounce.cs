f28e343ef2e8431cbf74b5de75cbeda4508f6abf
Ben Jacobs <ben.jacobs@vanti.co.uk>
2022-02-02 14:15:42 +0000

    docs: HasAnnounce -> IHasAnnounce

--- Bluetooth/IHasAnnounce.cs
+++ Bluetooth/IHasAnnounce.cs
@@ -1,6 +1,6 @@
 namespace AssessmentSysEng.Bluetooth {
   /// <summary>
-  /// HasAnnounce provides functionality for triggering this device to activate the pairing flow.
+  /// IHasAnnounce provides functionality for triggering this device to activate the pairing flow.
   /// After calling Announce, this device should be discoverable by Bluetooth sources to connect to using their
   /// native Bluetooth pairing interfaces.
   /// </summary>
