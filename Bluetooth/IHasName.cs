fb56c7e226cdfbf23f323620e001947c3694a044
Ben Jacobs <ben.jacobs@vanti.co.uk>
2022-02-02 14:16:01 +0000

    docs: HasName -> IHasName

--- Bluetooth/IHasName.cs
+++ Bluetooth/IHasName.cs
@@ -1,6 +1,6 @@
 namespace AssessmentSysEng.Bluetooth {
   /// <summary>
-  /// HasName defines the interface for getting a bluetooth devices announced name.
+  /// IHasName defines the interface for getting a bluetooth devices announced name.
   /// </summary>
   public interface IHasName {
     string Name();
